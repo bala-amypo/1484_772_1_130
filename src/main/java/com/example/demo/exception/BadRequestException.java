3513564aac9ff84500b67875e29025bd67dc7493
bala-amypo <bala@amypo.in>
2025-12-20 07:17:04 +0000

    User commit

--- src/main/java/com/example/demo/exception/BadRequestException.java
+++ src/main/java/com/example/demo/exception/BadRequestException.java
@@ -1,8 +1,24 @@
 package com.example.demo.exception;
 
-public class BadRequestException extends RuntimeException {
+public class BadRequestException extends IllegalArgumentException {
 
     public BadRequestException(String message) {
         super(message);
+    }
+
+    public static BadRequestException duplicateEmail() {
+        return new BadRequestException("Email already in use");
+    }
+
+    public static BadRequestException duplicateSerial() {
+        return new BadRequestException("Serial number already exists");
+    }
+
+    public static BadRequestException duplicateRule() {
+        return new BadRequestException("Rule already exists");
+    }
+
+    public static BadRequestException invalidInput() {
+        return new BadRequestException("Invalid input");
     }
 }
