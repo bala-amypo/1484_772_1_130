e26afe5cf95a2df8ba8b7424177036f36b7ceb8b
bala-amypo <bala@amypo.in>
2025-12-21 16:31:30 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -4,45 +4,45 @@ import com.example.demo.exception.InvalidRequestException;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 import java.util.Set;
+
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(
-            UserRepository userRepository,
-            PasswordEncoder passwordEncoder
-    ) {
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User registerUser(User user) {
+
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new InvalidRequestException("Email already in use");
         }
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         if (user.getRoles() == null || user.getRoles().isEmpty()) {
             user.setRoles(Set.of("USER"));
         }
+
         return userRepository.save(user);
     }
 
     @Override
     public User loginUser(String email, String password) {
+
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new NoSuchElementException("User not found"));
-        if (!passwordEncoder.matches(password, user.getPassword())) {
+
+        if (!user.getPassword().equals(password)) {
             throw new InvalidRequestException("Invalid input");
         }
+
         return user;
     }
 
