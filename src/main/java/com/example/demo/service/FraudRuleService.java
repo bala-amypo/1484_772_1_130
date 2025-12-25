fd38936783069e169d7842313103113a897c41a8
bala-amypo <bala@amypo.in>
2025-12-25 08:05:26 +0000

    User commit

--- src/main/java/com/example/demo/service/FraudRuleService.java
+++ src/main/java/com/example/demo/service/FraudRuleService.java
@@ -5,7 +5,7 @@ import com.example.demo.model.FraudRule;
 import java.util.List;
 import java.util.Optional;
 
-public interface FraudRulesService {
+public interface FraudRuleService {
     FraudRule createRule(FraudRule r);
     List<FraudRule> getActiveRules();
     Optional<FraudRule> getRuleByCode(String code);
