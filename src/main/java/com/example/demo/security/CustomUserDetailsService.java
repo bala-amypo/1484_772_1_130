8c99bcf0afd399b7785f20400fd1454347a6177e
bala-amypo <bala@amypo.in>
2025-12-25 09:02:25 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,9 +1,5 @@
 package com.example.demo.security;
 
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-
 public class CustomUserDetailsService {
+
 }
